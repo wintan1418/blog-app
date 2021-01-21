ac3f078ebf20c8a64d48118157bc28f3e4afaf41
oluwadare juwon <wintan1418@gmail.com>
2021-01-21 16:44:56 +0100

    fix linter errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: 'dhh', password: 'secret', only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
@@ -16,10 +15,8 @@ class CommentsController < ApplicationController
   end
 
   private
-    def comment_params
-      params.require(:comment).permit(:commenter, :body, :status)
-    end
 
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 end
-
-  
